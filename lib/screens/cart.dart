a3983095e3b881fad8c4d7d7f0c6d3079832c256
phansa <lekgaetonic@gmail.com>
2020-07-31 20:19:51 +0700

    cart

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -19,8 +19,160 @@ class _CartState extends State<Cart> {
     print('cart --> build');
     return Scaffold(
       appBar: Header(),
-      body: Text("cart"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 200,
+              height: 70,
+              child: TextField(
+                maxLength: 1,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Row',
+                ),
+                onChanged: (value) => {
+                  setState(() {
+                    try {
+                      iRow = int.parse(value) ?? 1;
+                    } catch (error) {
+                      iRow = 1;
+                    }
+                  })
+                },
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 70,
+              child: TextField(
+                maxLength: 1,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Column',
+                ),
+                onChanged: (value) => {
+                  setState(() {
+                    try {
+                      iColumn = int.parse(value) ?? 1;
+                    } catch (error) {
+                      iColumn = 1;
+                    }
+                  })
+                },
+              ),
+            ),
+            Row(
+                children: List.generate(
+                    iRow,
+                    (index) => Container(
+                          decoration: BoxDecoration(
+                            color: Colors.yellowAccent,
+                            border: Border.all(
+                              color: Colors.black12,
+                              width: 1,
+                            ),
+                          ),
+                          child: Center(
+                              child: Text(
+                            (1 + index).toString(),
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )),
+                          width: 50,
+                          height: 50,
+                        ))),
+            Row(
+                children: List.generate(
+                    iColumn,
+                    (index) => Column(
+                        children: List.generate(
+                            iColumn,
+                            (index2) => Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.redAccent,
+                                    border: Border.all(
+                                      color: Colors.black12,
+                                      width: 1,
+                                    ),
+                                  ),
+                                  child: Center(
+                                      child: Text(
+                                    (1 + index2).toString(),
+                                  )),
+                                  width: 50,
+                                  height: 50,
+                                ))))),
+            Row(
+                children: List.generate(
+                    iRow,
+                    (index) => Container(
+                          decoration: BoxDecoration(
+                            color: Colors.greenAccent,
+                            border: Border.all(
+                              color: Colors.black12,
+                              width: 1,
+                            ),
+                          ),
+                          child: Center(
+                              child: Text(
+                            (1 + index).toString(),
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )),
+                          width: 50,
+                          height: 50,
+                        ))),
+            Row(
+                children: List.generate(
+                    iColumn,
+                    (index) => Column(
+                        children: List.generate(
+                            iColumn,
+                            (index2) => Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.blueAccent,
+                                    border: Border.all(
+                                      color: Colors.black12,
+                                      width: 1,
+                                    ),
+                                  ),
+                                  child: Center(
+                                      child: Text(
+                                    (1 + index2).toString(),
+                                  )),
+                                  width: 50,
+                                  height: 50,
+                                ))))),
+            Row(
+                children: List.generate(
+                    iRow,
+                    (index) => Container(
+                          decoration: BoxDecoration(
+                            color: Colors.orangeAccent,
+                            border: Border.all(
+                              color: Colors.black12,
+                              width: 1,
+                            ),
+                          ),
+                          child: Center(
+                              child: Text(
+                            (1 + index).toString(),
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )),
+                          width: 50,
+                          height: 50,
+                        ))),
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigation(),
     );
   }
+
+  var iRow = 1;
+  var iColumn = 1;
 }
